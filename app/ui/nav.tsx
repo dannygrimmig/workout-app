e84c72946eeb8f3cd82949ad7c59c0cc9e942c58
DannyGrimmig <dannygrimmig17@gmail.com>
2024-05-08 11:41:47 -0400

    quick nav fix

--- app/ui/nav.tsx
+++ app/ui/nav.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 export function NavBar() {
   return (
     <nav className="z-10 sticky top-0 bg-slate-100 border-b border-black py-4 px-2 sm:px-8 flex items-center justify-between">
-      <div className="flex gap-4 sm:gap-16 items-center">
+      <div className="flex gap-4 sm:gap-8 md:gap-16 items-center">
         <Link href="/">
           <h1 className="text-2xl sm:text-4xl font-thin">The Logs &#x1f3cb;</h1>
         </Link>
-        <ul className="flex gap-2 sm:gap-8">
+        <ul className="flex gap-2 sm:gap-4 md:gap-8">
           {LINKS.map((link) => (
             <Link key={link} href={`/${link}`}>
               {link}
